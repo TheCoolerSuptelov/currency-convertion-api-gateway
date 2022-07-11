7a963da6c74b572bebd2e3e70b65e540499ce728
kirill.suptelov <kirill.suptelov@gmail.com>
2022-07-11 22:44:40 +0700

    write short url router 	modified:   src/main/java/com/github/thecoolersuptelov/apigateway/ApiGatewayConfiguration.java

--- src/main/java/com/github/thecoolersuptelov/apigateway/ApiGatewayConfiguration.java
+++ src/main/java/com/github/thecoolersuptelov/apigateway/ApiGatewayConfiguration.java
@@ -1,23 +1,26 @@
 package com.github.thecoolersuptelov.apigateway;
 
-import org.springframework.cloud.gateway.route.Route;
+
 import org.springframework.cloud.gateway.route.RouteLocator;
-import org.springframework.cloud.gateway.route.builder.Buildable;
-import org.springframework.cloud.gateway.route.builder.PredicateSpec;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-
-import java.util.function.Function;
 
 @Configuration
 public class ApiGatewayConfiguration {
 
     @Bean
-    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder){
-       return routeLocatorBuilder.routes()
-                .route(p->p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+    public RouteLocator gatewayRouter(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p -> p.path("/currency-exchange/**").uri("lb://currency-exchange"))
+                .route(p -> p.path("/currency-conversion-service/**").uri("lb://currency-conversion-service"))
+                .route(p -> p.path("/currency-conversion-service-feign/**").uri("lb://currency-conversion-service"))
+                .route(p->p.path("/cc/**")
+                        .filters(f->f.rewritePath(
+                                "/cc/(?<segment>.*)",
+                                "/currency-conversion-service/${segment}"))
+                        .uri("lb://currency-conversion-service")
+                )
                 .build();
     }
 
